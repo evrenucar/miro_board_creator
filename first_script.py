a34a0a0ecdbf0eaef41bfee2c18b7f1e2504bce7
evrenucar <evrenucar1999@gmail.com>
2022-01-24 21:37:26 +0300

    can finally log into miro

--- first_script.py
+++ first_script.py
@@ -19,11 +19,12 @@ email_field.send_keys(Keys.TAB)
 password_field = driver.find_element_by_name("password")
 password_field.send_keys(password) 
 password_field.send_keys(Keys.TAB)
-password_field.send_keys(Keys.TAB)
-driver.findElement(By.tagName("body")).sendKeys(Keys.TAB)
+driver.find_element_by_class_name("signup__submit").click()
+
 time.sleep(8)
+
 # email = driver.find_element_by_name("email")
 # email.send_keys("evrenucar1999@gmail.com")
 
-driver.quit()
+# driver.quit()
 
